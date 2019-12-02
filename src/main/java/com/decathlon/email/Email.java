07ee4bb2a649f4e4def7557e0d715af1c253ecbd
Matthew_Saw <matthew.saw@decathlon.com>
2019-12-02 16:15:25 +0100

    Init

---
+++ src/main/java/com/decathlon/email/Email.java
@@ -0,0 +1,26 @@
+package com.decathlon.email;
+
+public class Email {
+
+  private String firstName;
+  private String lastName;
+  private String password;
+  private String department;
+  private String alternateEmail;
+  private int mailboxCapacity;
+
+  //Constructor ot receive first name last name.
+
+  //ask for department
+
+  //Generate password
+
+  //Set mailbox capacity
+
+  //Set alternate email address
+
+  //Change password
+
+
+
+}
