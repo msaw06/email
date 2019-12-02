7044d349d109b3113e545305386d44c25278654b
Matthew_Saw <matthew.saw@decathlon.com>
2019-12-02 17:40:39 +0100

    Init continued

--- src/main/java/com/decathlon/email/Email.java
+++ src/main/java/com/decathlon/email/Email.java
@@ -1,5 +1,7 @@
 package com.decathlon.email;
 
+import java.util.Scanner;
+
 public class Email {
 
   private String firstName;
@@ -8,19 +10,50 @@ public class Email {
   private String department;
   private String alternateEmail;
   private int mailboxCapacity;
+  private int defaultPasswordLength = 10;
 
   //Constructor ot receive first name last name.
+  Email(String firstName, String lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+
+    this.department = setDepartment();
+    System.out.println("Department is " + this.department);
+
+    this.password = randomPassword(defaultPasswordLength);
+    System.out.println("Your password is " + this.password);
+  }
 
   //ask for department
+  private String setDepartment() {
+    System.out.print("Departments:\n1 Sales\n2 Marketing\n3 Accounting \n0 None\nEnter department:");
+    Scanner in = new Scanner(System.in);
+    int departmentChoice = in.nextInt();
+    if (departmentChoice == 1) {
+      return "Sales";
+    } else if (departmentChoice == 2) {
+      return "Marketing";
+    } else if (departmentChoice == 3) {
+      return "Accounting";
+    } else {
+      return "";
+    }
+  }
+
 
   //Generate password
+  private String randomPassword(int length) {
+    String passwordSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#$@";
+    char[] password = new char[length];
+    for (int i = 0; i < length; i++) {
+      int rand = (int) (Math.random() * passwordSet.length());
+      password[i] = passwordSet.charAt(rand);
+    }
+    return new String(password);
+  }
+//Set mailbox capacity
 
-  //Set mailbox capacity
+//Set alternate email address
 
-  //Set alternate email address
-
-  //Change password
-
-
-
+//Change password
 }
--- src/main/java/com/decathlon/email/EmailApplication.java
+++ src/main/java/com/decathlon/email/EmailApplication.java
@@ -1,13 +1,11 @@
 package com.decathlon.email;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class EmailApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(EmailApplication.class, args);
+
+    //SpringApplication.run(EmailApplication.class, args);
+    Email email1 = new Email("Matthew", "Saw");
   }
 
 }
